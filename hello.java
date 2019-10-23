e2e49a666d5d202dff8a6f380c4611b52abf71d8
NotEnoughMilk <31776017+NotEnoughMilk@users.noreply.github.com>
2019-10-23 09:15:47 -0400

    Just added my name and stuff :)

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Jared";
+        String lastName = "Rand";
 	
-        System.out.println("Hello Class!");
+        System.out.printf("Hello, my name is %s %s!", firstname, lastname);
 
 
 
